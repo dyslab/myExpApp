3c9e50d716016e04a5082cc6f91b1c6bc1aa8a04
45236395+dyslab@users.noreply.github.com <45236395+dyslab@users.noreply.github.com>
2018-12-23 20:42:39 +0800

    add a game for fun

--- public/javascripts/racinggame.js
+++ public/javascripts/racinggame.js
@@ -1,7 +1,45 @@
 $(document).ready(function () {
+    function racing() {
+        var nexttr = Math.floor(Math.random() * 1000)
+        setTimeout(() => {
+            $.get('/racinggame/racing', (data) => {
+                var tno = Math.floor(Math.random() * 4)
+                $('span#spTrack:eq(' + tno + ')').append(data)
+                var cw = $('span#spTrack:eq(' + tno + ')').width()
+                var pw = $('span#spTrack:eq(' + tno + ')').parent().width()
+                if (cw + 30 >= pw) stop(tno)
+                else racing()
+            })
+        }, nexttr)
+    }
+
+    function stop(winner) {
+        var result
+        var msg = 'The winner is ' 
+                + $("input[name=rdBet]:radio:eq(" + winner + ")").val() 
+                + '. You bet on ' + $("input[name=rdBet]:radio:checked").val()
+        if ($("input[name=rdBet]:radio:eq(" + winner + ")").val() === $("input[name=rdBet]:radio:checked").val()) {
+            msg += '\n\nYou won.'
+            result = 'win'
+        } else {
+            msg += '\n\nYou lost.'
+            result = 'lose'
+        }
+
+        $.get('/racinggame/stop', { result: result }, (data) => {
+            window.alert(msg)
+            $("#divResult").append(data)
+            $("#btnStart").show()
+        })
+    }
+
     $("#btnStart").click(function () {
         if ($("input[name=rdBet]:radio:checked").val() !== undefined) {
-            window.alert("Boss, we are ready go now.")
+            $.get('/racinggame/start', (data) => {
+                $('span#spTrack').text('')
+                $("#btnStart").hide()
+                racing();
+            })
         } else {
             window.alert("Boss, you didn't select a track.")
         }
--- routes/racinggame-server.js
+++ routes/racinggame-server.js
@@ -1,8 +1,69 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var fs = require('fs')
 
+// For Interval function
+var trInterval, countInterval
+
+// -----------------------------------------------------------------------------------------------
+// Get filename by 'no'
+function constructFilename(no) {
+  return './public/bufferdata/racing' + no + '.txt'
+}
+
+// Interval writing random file with random number
+function startIntervalWritingFile() {
+  return setInterval(() => {
+    let fno = Math.floor(Math.random() * 3 + 1)
+    let rc = Math.round(Math.random() * 100)
+    fs.writeFile(constructFilename(fno), rc, (err) => {
+      if (err) console.log(err)
+    })
+    countInterval ++;
+    // console.log(countInterval + '#' + fno + '$' + rc)
+    if (countInterval > 99) {
+      clearInterval(trInterval)
+    }
+  }, 1000)
+}
+
+// Read random file
+function readRandomFile() {
+  let fno = Math.floor(Math.random() * 3 + 1)
+
+  return fno + '#' + fs.readFileSync(constructFilename(fno)) + '$'
+}
+
+// Return result
+function getResult(result) {
+  return '<img src="/images/' + result + '.svg">'
+}
+
+// -----------------------------------------------------------------------------------------------
+// GET first page
 router.get('/', function(req, res, next) {
   res.render('racinggame', { title: 'Racing Gambling Game' });
-});
+})
 
-module.exports = router;
+// GET start
+router.get('/start', function(req, res, next) {
+  // Start interval writing file
+  countInterval = 0
+  trInterval = startIntervalWritingFile()
+  res.send('start')
+})
+
+// GET racing
+router.get('/racing', function(req, res, next) {
+  // Read random file
+  res.send(readRandomFile())
+})
+
+// GET stop
+router.get('/stop', function(req, res, next) {
+  // Clear interval 
+  clearInterval(trInterval)
+  res.send(getResult(req.query.result))
+})
+
+module.exports = router
