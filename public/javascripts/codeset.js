00100db8c600d31ea74e7817243506890bebb129
Vincent To <vincent_to@139.com>
2024-12-10 00:21:47 +0800

    Add module: fetching qr code via public api 📡

--- public/javascripts/codeset.js
+++ public/javascripts/codeset.js
@@ -36,3 +36,17 @@ function submitBarcode() {
 
     formA.submit();
 }
+
+async function fetchQRcodeViaPublicApi() {
+    const strQRCode = document.getElementById("strQRCode").value;
+    const intQRCodeSize = 180;
+    const resp = await fetch(`https://api.apgy.in/qr/?data=${encodeURIComponent(strQRCode)}&size=${intQRCodeSize}`);
+    if (resp.ok) {
+        const blob = await resp.blob();
+        const img = document.getElementById("imgApiQRCode");
+        img.src = URL.createObjectURL(blob);
+    } else {
+        console.log(resp);
+        throw new Error("Network response was not ok");
+    }
+}
